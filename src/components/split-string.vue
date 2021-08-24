644d4fa274594577d017d169e26d2eeda4153fed
Levi Ceccato <levi.ceccato@gmail.com>
2021-08-24 17:00:11 +1000

    Implement split string animation

--- src/components/split-string.vue
+++ src/components/split-string.vue
@@ -1,7 +1,8 @@
 <script>
-import { computed } from 'vue'
+import { computed, watch, ref } from 'vue'
 import { useStore } from 'nanostores/vue'
 import { textRotation, textSkew } from '../scripts/store.js'
+import { throttle } from 'lodash-es'
 
 export default {
     props: {
@@ -11,33 +12,38 @@ export default {
         const getTextRotation = useStore(textRotation)
         const getTextSkew = useStore(textSkew)
 
-        const splitLines = computed(() =>
-            props.lines.map((line) =>
-                line.split('').map((char) => char === ' ' ? '&nbsp' : char)
-            )
-        )
+        const rotations = ref([]) 
+        const skews = ref([])
 
-        const transformMap = computed(() => {
-            const asd = splitLines.value.map((line, lineIndex) => {
-                return line.map((char, charIndex) => {
-                    return getTextRotation.value
-                })
-            })
-            console.log(asd)
-            return asd
-        })
+        const lineData = computed(() => props.lines.map((line) => line.split('').map((char) => char === ' ' ? '&nbsp' : char)))
+        const maxLineLength = computed(() => Math.max(...lineData.value.map(line => line.length)))
         
+        const cycleArray = (array, to) => {
+            if (array.value.length > maxLineLength.value) array.value.pop()
+            throttle(() => array.value.unshift(to), 10)()
+        }
 
-        return { splitLines, transformMap }
+        watch(getTextRotation, (to) => cycleArray(rotations, to))
+        watch(getTextSkew, (to) => cycleArray(skews, to))
+
+        return { lineData, rotations, skews }
     },
 }
 </script>
 
 <template>
-    <template v-for="line in splitLines">
+    <template v-for="line in lineData">
         <span
             class="char"
             v-for="(char, index) in line"
+            :data-index="index" 
+            :style="{
+                transform: `
+                    translateY(${((rotations[index] || 0) * 5) * -1}px)
+                    rotateZ(${(rotations[index] || 0) * 1}deg)
+                    skew(${(skews[index] || 0) * 1}deg)
+                `
+            }"
             v-html="char">
         </span>
         <br />
