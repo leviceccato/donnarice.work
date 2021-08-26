94d1a72a9d36fa1f59d15703665ab1ae221d0d5e
Levi Ceccato <levi.ceccato@gmail.com>
2021-08-26 23:34:28 +1000

    Add slide animations

--- src/components/loader.vue
+++ src/components/loader.vue
@@ -38,25 +38,42 @@ export default {
 </script>
 
 <template>
-    <div :class="['loader', areImagesLoaded && 'open']">
-        <div class="slide top" />
-        <div class="bar left" :style="{ transform: `translateX(-${barOffsetPercentage}%)` }" />
-        <div class="bar right" :style="{ transform: `translateX(${barOffsetPercentage}%)` }" />
-        <div class="slide bottom" />
-    </div>
+<div :class="['loader', areImagesLoaded && 'open']">
+    <div class="slide top" />
+    <div class="bar left" :style="{ transform: `translateX(-${barOffsetPercentage}%)` }" />
+    <div class="bar right" :style="{ transform: `translateX(${barOffsetPercentage}%)` }" />
+    <div class="slide bottom" />
+</div>
 </template>
 
 <style lang="scss" scoped>
 .loader {
+    pointer-events: none;
     position: fixed;
     z-index: 2;
+    top: 0;
+    left: 0;
+    right: 0; 
+    bottom: 0;
+    &.open {
+        .slide {
+            &.top {
+                transform: translateY(-100%);
+            }
+            &.bottom {
+                transform: translateY(100%);
+            }
+        }
+    }
 }
 .slide {
     position: absolute;
     left: 0;
     width: 100%;
-    background-color: var(--col-bg);
+    background-color: var(--col-fg);
     height: calc(50% - 0.5px);
+    transition: transform 650ms cubic-bezier(0.83, 0, 0.17, 1); // Ease in  quint 
+    transition-delay: 450ms;
     &.top {
         top: 0;
     }
@@ -69,7 +86,7 @@ export default {
     width: 50%;
     height: 1px;
     transition: transform 150ms;
-    background-color: var(--col-bg);
+    background-color: var(--col-fg);
     top: calc(50% - 0.5px);
     &.left {
         left: 0;
