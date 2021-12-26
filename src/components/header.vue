d173458f78abd603da83c1953ca705b18492cada
Levi <levi@coassemble.com>
2021-12-26 22:41:10 +1100

    Get some semblance of something

---
+++ src/components/header.vue
@@ -0,0 +1,19 @@
+<template>
+    <header :class="$style.header">
+        <a
+            :class="$style.link"
+            href="/"
+        >
+            Home
+        </a>
+    </header>
+</template>
+
+<style lang="scss" module>
+.header {
+    border: 1px solid red;
+}
+.link {
+    color: green;
+}
+</style>
---
+++ src/components/layout-default.vue
@@ -0,0 +1,15 @@
+<script>
+import Header from './header.vue'
+
+export default {
+    name: 'LayoutDefault',
+    components: { Header }
+}
+</script>
+
+<template>
+    <div>
+        <Header />
+        <slot />
+    </div>
+</template>
