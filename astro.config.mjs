10a8baf37ecc36e038b1526ff5dc2e5168e97119
Levi <levi@coassemble.com>
2021-12-26 18:44:14 +1100

    Reference package correctly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 export default {
     // Move into 'src' folder to keep root tidy
     public: './src/static',
-    renderers: ['astrojs/renderer-vue'],
+    renderers: ['@astrojs/renderer-vue'],
     buildOptions: {
         site: 'https://donnarice.work'
     }
