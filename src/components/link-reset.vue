3a9c39d77eaa82233945f15abb54649bff8c918b
Levi <levi@coassemble.com>
2021-12-28 09:21:52 +1100

    Add link reset styles

---
+++ src/components/link-reset.vue
@@ -0,0 +1,20 @@
+<template>
+    <a :class="$style.link">
+        <slot />
+    </a>
+</template>
+
+<style lang="scss" module>
+.link {
+    color: inherit;
+    text-decoration: inherit;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+    -webkit-tap-highlight-color: transparent;
+    &:focus {
+        outline: none;
+    }
+    &:focus-visible {
+        box-shadow: 0px 0px 0px 2px currentColor;
+    }
+}
+</style>
