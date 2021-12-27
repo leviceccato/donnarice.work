3eb46723b52a999651054a877a3caf3a4fbd5ddd
Levi <levi@coassemble.com>
2021-12-27 21:40:19 +1100

    Add header with links and toggle button

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,17 +1,77 @@
+<script setup>
+import { reactive } from 'vue'
+
+import ButtonReset from './button-reset.vue'
+
+const state = reactive({
+    isNavOpen: false
+})
+</script>
+
 <template>
-    <header :class="$style.header">
-        <a
-            :class="$style.link"
-            href="/"
+    <div role="banner" :class="$style.header">
+        <ButtonReset
+            :class="$style.button"
+            @click="state.isNavOpen = !state.isNavOpen"
         >
-            Home
-        </a>
-    </header>
+            {{ state.isNavOpen ? 'Close menu' : 'Open menu' }}
+        </ButtonReset>
+        <nav :class="[$style.nav, { [$style.open]: state.isNavOpen }]">
+            <a
+                :class="$style.link"
+                href="#intro"
+            >
+                Intro
+            </a>
+            <a
+                :class="$style.link"
+                href="#work"
+            >
+                Work
+            </a>
+            <a
+                :class="$style.link"
+                href="#testimonials"
+            >
+                Testimonials
+            </a>
+            <a
+                :class="$style.link"
+                href="#resume"
+            >
+                Resume
+            </a>
+            <a
+                :class="$style.link"
+                href="#contact"
+            >
+                Contact
+            </a>
+        </nav>
+    </div>
 </template>
 
 <style lang="scss" module>
+@use '../styles/utilities.scss' as *;
+
 .header {
-    border: 1px solid red;
+    display: flex;
+    flex-direction: column;
+    .button {
+        @include media(s) {
+            display: none;
+        }
+    }
+}
+.nav {
+    display: none;
+    flex-direction: column;
+    @include media(s) {
+        display: flex;
+    }
+    &.open {
+        display: flex;
+    }
 }
 .link {
     color: green;
