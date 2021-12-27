f5ab9be62a33018dc263ae764db9ba63ccf15d53
Levi <levi@coassemble.com>
2021-12-28 09:22:13 +1100

    Add text component that includes line height cropping

---
+++ src/components/text.vue
@@ -0,0 +1,18 @@
+<template>
+    <span :class="$style.text">
+        <slot />
+    </span>
+</template>
+
+<style lang="scss" module>
+.text {
+    display: block;
+    &::before {
+        content: '';
+        display: block;
+        height: 0;
+        width: 0;
+        margin-top: calc((var(--capital-height, 0.7) - var(--line-height)) * 0.5em);
+    }
+}
+</style>
