3883ae6175668fe057e8fe563d1f518442cbcd5c
Levi <levi@coassemble.com>
2021-12-27 21:40:01 +1100

    Add button reset component

---
+++ src/components/button-reset.vue
@@ -0,0 +1,24 @@
+<template>
+    <button :class="$style.button">
+        <slot />
+    </button>
+</template>
+
+<style lang="scss" module>
+.button {
+    cursor: pointer;
+    color: inherit;
+    background: none;
+    border: none;
+    font-family: inherit;
+    text-align: inherit;
+    font-size: inherit;
+    letter-spacing: inherit;
+    padding: 0;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+    -webkit-tap-highlight-color: transparent;
+    &:focus {
+        outline: none;
+    }
+}
+</style>
