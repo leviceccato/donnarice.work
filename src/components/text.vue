1659fee24938e70cee4ca6b7915487d091b780a0
Levi <levi@coassemble.com>
2021-12-28 10:27:09 +1100

    Allow overriding crop values

--- src/components/text.vue
+++ src/components/text.vue
@@ -1,5 +1,40 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+    crop: { type: [String, Array] }
+})
+
+const style = computed(() => {
+    if (typeof props.crop === 'string') {
+        if (props.crop === 'uppercase') {
+            return {
+                '--cap-height': '0.5',
+                '--x-desc-height': '0.72'
+            }
+        }
+    }
+
+    // Array
+    if (typeof props.crop === 'object') {
+        return {
+            '--cap-height': String(props.crop[0]),
+            '--x-desc-height': String(props.crop[1])
+        }
+    }
+
+    return {
+        '--cap-height': '0.5',
+        '--x-desc-height': '1.15'
+    }
+})
+</script>
+
 <template>
-    <span :class="$style.text">
+    <span
+        :class="$style.text"
+        :style="style"
+    >
         <slot />
     </span>
 </template>
@@ -7,12 +42,18 @@
 <style lang="scss" module>
 .text {
     display: block;
-    &::before {
+    &::before,
+    &::after {
         content: '';
         display: block;
         height: 0;
         width: 0;
-        margin-top: calc((var(--capital-height, 0.7) - var(--line-height)) * 0.5em);
+    }
+    &::before {
+        margin-top: calc((var(--cap-height, 0.5) - var(--line-height)) * 0.5em);
+    }
+    &::after {
+        margin-bottom: calc((var(--x-desc-height, 0.72) - var(--line-height)) * 0.5em);
     }
 }
 </style>
