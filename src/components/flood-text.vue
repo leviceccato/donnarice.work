da1c57223701dc180e289ba7cb1c03fd5dfbdc35
Levi <levi@coassemble.com>
2021-12-28 18:22:37 +1100

    Add flood text main behaviours

--- src/components/flood-text.vue
+++ src/components/flood-text.vue
@@ -1,25 +1,58 @@
 <script setup>
-import { computed } from 'vue'
+import { computed, reactive } from 'vue'
 
 const props = defineProps({
     text: { type: String, default: '' }
 })
 
-const splitText = computed(() => {
-    let result = '';
+const state = reactive({
+    hoveredIndex: -1
+})
 
-    props.text.split('').forEach(char => {
-        let segment = char
+const segments = computed(() => {
+    return props.text.split('').map(char => {
         if (char === ' ') {
-            segment = '&nbsp;'
+            char = '&nbsp;'
         }
-        result += `<span>${segment}</span>`
+        return char
     })
+})
 
-    return result;
+const delays = computed(() => {
+    return segments.value.map((_, index) => {
+        const offset = Math.abs(state.hoveredIndex - index)
+        return offset * 50
+    })
 })
 </script>
 
 <template>
-    <span v-html="splitText" />
+    <span :class="$style.segments">
+        <span
+            v-for="segment, index in segments"
+            :key="index"
+            :class="$style.segment"
+            v-html="segment"
+            :style="{ transitionDelay: `${delays[index]}ms` }"
+            @mouseover="state.hoveredIndex = index"
+        />
+    </span>
 </template>
+
+<style lang="scss" module>
+.segments {
+    &:hover {
+        .segment {
+            text-shadow:
+                -0.6px 0px 0 currentColor,
+                0px -0.6px 0 currentColor,
+                0.6px 0px 0 currentColor,
+                0px 0.6px 0 currentColor;
+        }
+    }
+}
+.segment {
+    transition: text-shadow 350ms ease;
+    display: inline-block;
+}
+</style>
