0330d42ad1d618a6492abf874cb484cf5ec9ce77
Levi <levi@coassemble.com>
2021-12-28 12:55:11 +1100

    Create flood text effect component

---
+++ src/components/flood-text.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+    text: { type: String, default: '' }
+})
+
+const splitText = computed(() => {
+    let result = '';
+
+    props.text.split('').forEach(char => {
+        let segment = char
+        if (char === ' ') {
+            segment = '&nbsp;'
+        }
+        result += `<span>${segment}</span>`
+    })
+
+    return result;
+})
+</script>
+
+<template>
+    <span v-html="splitText" />
+</template>
