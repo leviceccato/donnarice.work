1f67b64694748d4892a386750511b1f06e0133ef
Levi <levi@coassemble.com>
2021-12-28 11:49:30 +1100

    Add generic link component

---
+++ src/components/link.vue
@@ -0,0 +1,17 @@
+<script setup>
+import LinkReset from './link-reset.vue'
+</script>
+
+<template>
+    <LinkReset :class="$style.link">
+        <slot />
+    </LinkReset>
+</template>
+
+<style lang="scss" module>
+.link.link {
+    display: inline-flex;
+    padding: 0.2em;
+    margin: -0.2em;
+}
+</style>
