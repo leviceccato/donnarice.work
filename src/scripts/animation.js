af58c099e3a7536106a056367a2df84cf2041dce
Levi <levi@coassemble.com>
2021-12-31 09:27:17 +1100

    Remove semicolons

--- src/scripts/animation.js
+++ src/scripts/animation.js
@@ -1,22 +1,22 @@
-export const easeInOutSine = x => -(Math.cos(Math.PI * x) - 1) / 2;
+export const easeInOutSine = x => -(Math.cos(Math.PI * x) - 1) / 2
 
 export const animate = (from, to, duration, ease, callback) => {
     return new Promise(resolve => {
-        let start, stop;
+        let start, stop
         const startAnimation = timestamp => {
-            start = timestamp;
-            draw(timestamp);
-        };
+            start = timestamp
+            draw(timestamp)
+        }
         const draw = now => {
             if (stop) {
-                resolve();
-                return callback(to);
+                resolve()
+                return callback(to)
             }
-            if (now - start >= duration) stop = true;
-            const delta = (now - start) / duration;
-            callback(from + (to - from) * ease(delta), stop);
-            window.requestAnimationFrame(draw);
-        };
-        window.requestAnimationFrame(startAnimation);
-    });
-};
+            if (now - start >= duration) stop = true
+            const delta = (now - start) / duration
+            callback(from + (to - from) * ease(delta), stop)
+            window.requestAnimationFrame(draw)
+        }
+        window.requestAnimationFrame(startAnimation)
+    })
+}
