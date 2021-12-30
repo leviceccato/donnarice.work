ca6611a4f74868f683601a91605a974d67d1a20f
Levi <levi@coassemble.com>
2021-12-30 19:56:38 +1100

    Add virtual link capabilities

--- src/components/link.vue
+++ src/components/link.vue
@@ -1,9 +1,34 @@
 <script setup>
+import { computed, inject, useAttrs } from 'vue'
+
 import LinkReset from './link-reset.vue'
+
+const props = defineProps({
+    isVirtual: { type: Boolean, default: false }
+})
+
+const emit = defineEmits(['follow'])
+
+const attrs = useAttrs()
+
+const scrollContext = inject('scrollContext', null)
+
+const followHref = event => {
+    if (!props.isVirtual) return
+    if (!attrs.href) return
+    if (!scrollContext) return
+
+    event.preventDefault()
+    scrollContext.value.scrollTo(attrs.href)
+    emit('follow')
+}
 </script>
 
 <template>
-    <LinkReset :class="$style.link">
+    <LinkReset
+        :class="$style.link"
+        @click="followHref"
+    >
         <slot />
     </LinkReset>
 </template>
