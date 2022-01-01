eae3a5255239609c40af9a0061656282002f5c9c
Levi <levi@coassemble.com>
2022-01-01 13:57:20 +1100

    Ensure link follow animations don't mess with scrollbar

--- src/components/page-home.vue
+++ src/components/page-home.vue
@@ -16,20 +16,21 @@ onMounted(() => isReady.value = true)
 <template>
     <DynamicBackground v-slot="{ scrolling, scrollDuration }">
         <Header />
-        <main
-            style="height: 3000px"
-            :style="{ animationDuration: `${scrollDuration}ms` }"
-            :class="[$style.main, {
-                [$style.scrollingDown]: scrolling === 'down',
-                [$style.scrollingUp]: scrolling === 'up'
-            }]"
-        >
-            <Hero id="intro" />
-            <Hero id="work" />
-            <Hero id="testimonials" />
-            <Hero id="resume" />
-            <Hero id="contact" />
-        </main>
+        <div :class="$style.container">
+            <main
+                :style="{ animationDuration: `${scrollDuration}ms` }"
+                :class="[$style.main, {
+                    [$style.scrollingDown]: scrolling === 'down',
+                    [$style.scrollingUp]: scrolling === 'up'
+                }]"
+            >
+                <Hero id="intro" />
+                <Hero id="work" />
+                <Hero id="testimonials" />
+                <Hero id="resume" />
+                <Hero id="contact" />
+            </main>
+        </div>
     </DynamicBackground>
 </template>
 
@@ -48,6 +49,10 @@ onMounted(() => isReady.value = true)
     100% {
         transform: translateY(0);
     }
+}
+
+.container {
+    overflow: hidden;
 }
 
 .main {
