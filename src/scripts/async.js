d06dbb424ac9d6a263fe4bbf1e37560fcfc61137
Levi <levi@coassemble.com>
2022-01-02 11:56:25 +1100

    Formatting

--- src/scripts/async.js
+++ src/scripts/async.js
@@ -2,7 +2,7 @@ export const sleep = wait => new Promise(resolve => setTimeout(resolve, wait))
 
 export const debounce = function(wait, func) {
     let timeout
-    return function () {
+    return function() {
         clearTimeout(timeout)
         timeout = setTimeout(() => func.apply(this, arguments), wait)
     }
