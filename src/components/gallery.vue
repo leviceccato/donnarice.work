3b303c4f28a9e525745c40e75a9f99edc0c4bbd5
Levi <levi@coassemble.com>
2022-01-02 16:28:30 +1100

    Add WIP Gallery component

---
+++ src/components/gallery.vue
@@ -0,0 +1,41 @@
+<script setup>
+const props = defineProps({
+    items: { type: Array, default: () => [] }
+})
+</script>
+
+<template>
+    <div :class="$style.gallery">
+        <div :class="$style.container">
+            <div
+                v-for="item, index in items"
+                :key="index"
+                :class="$style.item"
+            >
+                <figure :class="$style.imageContainer">
+                    <img
+                        :class="$style.image"
+                        :src="item.image"
+                        :alt="item.imageAlt || ''"
+                    />
+                </figure>
+                <div :class="$style.textContainer">
+                    <div
+                        :class="$style.text"
+                        v-html="item.text"
+                    />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" module>
+.gallery {
+
+}
+
+.container {
+
+}
+</style>
