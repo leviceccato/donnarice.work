6faff687663adba5ae82b3ce9115864c7c3cf6af
Levi <levi@coassemble.com>
2022-01-03 21:04:56 +1100

    Add unique id component

---
+++ src/components/unique-id.vue
@@ -0,0 +1,20 @@
+<script>
+import { ref } from 'vue'
+
+let uuid = 0
+
+export default {
+    setup() {
+        const id = ref(`unique-id-${uuid}`)
+        uuid++
+
+        return {
+            id
+        }
+    }
+}
+</script>
+
+<template>
+    <slot v-bind="{ id }" />
+</template>
