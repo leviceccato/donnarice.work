9ef77667fb3e0eb0afba5fdd10114860a62942fc
Levi <levi@coassemble.com>
2022-01-03 22:29:24 +1100

    Refactor so this actually works

--- src/components/unique-id.vue
+++ src/components/unique-id.vue
@@ -1,10 +1,16 @@
 <script>
+import { onMounted, ref } from 'vue'
+
 let uuid = 0
 
 export default {
     setup() {
-        const id = `unique-id-${uuid}`
-        uuid++
+        let id = ref('')
+
+        onMounted(() => {
+            id.value = `unique-id-${uuid}`
+            uuid++
+        })
 
         return { id }
     }
