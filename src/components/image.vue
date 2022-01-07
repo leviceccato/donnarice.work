23c404cb24adebb1efffeaf665c377617b18a1f7
Levi Ceccato <levi.ceccato@gmail.com>
2022-01-07 21:46:25 +1100

    Prevent unecessary duplication

--- src/components/image.vue
+++ src/components/image.vue
@@ -5,6 +5,10 @@ import { animate, easeInOutSine } from '../scripts/animation.js'
 import ImgReset from './img-reset.vue'
 import UniqueId from './unique-id.vue'
 
+const maxErosionRadius = 3
+const minErosionRadius = 0
+const erosionAnimDuration = 550
+
 export default {
     inheritAttrs: false,
     components: { ImgReset, UniqueId },
@@ -12,10 +16,6 @@ export default {
         isShown: { type: Boolean, default: false }
     },
     setup(props, { attrs }) {
-        const maxErosionRadius = 3
-        const minErosionRadius = 0
-        const erosionAnimDuration = 550
-
         const erosionRadius = ref(maxErosionRadius)
 
         watch(() => props.isShown, isShown => {
