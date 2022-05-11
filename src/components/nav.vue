681939a30093a0452e383f062895d5aa0a4974ee
Levi Ceccato <levi.ceccato@gmail.com>
2022-05-11 22:58:15 +1000

    Add nav component

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,3 +1,28 @@
 <template>
-    <span />
+    <nav :class="$style.root">
+        <a
+            href="#home"
+            :class="$style.link"
+        >
+            Home
+        </a>
+        <a
+            href="#work"
+            :class="$style.link"
+        >
+            Work
+        </a>
+        <a
+            href="#kind-words"
+            :class="$style.link"
+        >
+            Kind words
+        </a>
+    </nav>
 </template>
+
+<style lang="scss" module>
+.root {
+
+}
+</style>
