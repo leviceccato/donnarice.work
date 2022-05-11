0583ccf4927664794d33ba8d9e72a9cb448421b2
Levi Ceccato <levi.ceccato@gmail.com>
2022-05-11 22:20:13 +1000

    Add text

--- src/components/section-hero.vue
+++ src/components/section-hero.vue
@@ -6,7 +6,21 @@ import Text from './text.vue'
     <div :class="$style.root">
         <div :class="$style.column" />
         <div :class="[$style.column, $style.main]">
-            <Text variant="heading">Donna Rice</Text>
+            <Text variant="heading-small">Donna Rice</Text>
+            <Text
+                variant="heading-large"
+                :class="$style.intro"
+            >
+                I’m a draftsperson and senior PA based in Newcastle — Australia.
+            </Text>
+            <Text
+                variant="body"
+                :class="$style.body"
+            >
+                I’ve worked as an ___ at ____, as a ____ and have experience doing ____.
+                I’m passionate about the design of interior spaces and love to surround myself with ____.<br /><br />
+                I’m open to new employment opportunities. Send me a message.
+            </Text>
         </div>
         <div :class="$style.column" />
     </div>
@@ -18,5 +32,11 @@ import Text from './text.vue'
     grid-auto-flow: column;
     grid-auto-columns: 1fr;
     grid-gap: 20px;
+}
+.intro {
+    margin-top: 72px;
+}
+.body {
+    margin-top: 72px;
 }
 </style>
