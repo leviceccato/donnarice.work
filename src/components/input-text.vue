307cb234341f26520a34f7a3857b5e87f164f575
Levi Ceccato <levi.ceccato@gmail.com>
2022-05-15 16:55:29 +1000

    Add generic text input component

---
+++ src/components/input-text.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+const {
+    type = 'input',
+} = defineProps<{
+    type?: 'input' | 'textarea'
+}>()
+</script>
+
+<template>
+    <Component
+        :is="type"
+        :class="$style.root"
+        :type="(type === 'input') ? 'text' : null"
+    />
+</template>
+
+<style lang="scss" module>
+@use '../styles/util.scss';
+
+.root {
+    border: none;
+    resize: vertical;
+    &:focus {
+        outline: none;
+    }
+    &::-webkit-resizer {
+        background-position: center;
+        background-repeat: no-repeat;
+        background-image: url(util.svg-encode('<svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 17"><path d="M11.48 4.75v6.74H4.74" stroke="' + util.var(col-black-1) + '" stroke-width="2" stroke-linecap="square"/><path d="M11.48 11.49 4.74 4.75" stroke="#000" stroke-width="2"/></svg>'));
+    }
+}
+</style>
