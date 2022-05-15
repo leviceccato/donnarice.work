f1d159ab05519e1ce9ae3df5a63c11215a153653
Levi Ceccato <levi.ceccato@gmail.com>
2022-05-15 16:55:16 +1000

    Add form button

---
+++ src/components/button-form.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import Button from './button.vue'
+</script>
+
+<template>
+    <Button>
+        <span :class="$style.container">
+            <slot />
+        </span>
+    </Button>
+</template>
+
+<style lang="scss" module>
+.container {
+    display: flex;
+    align-items: center;
+    gap: 1em;
+}
+</style>
