61c16940c1d6de33da3288aa8eedf372124556ef
Levi Ceccato <levi.ceccato@gmail.com>
2022-05-15 17:07:59 +1000

    Add custom cursor component

---
+++ src/components/cursor.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue'
+
+const x = ref(0)
+const y = ref(0)
+
+function setCursorCoords(event: MouseEvent): void {
+    x.value = event.clientX
+    y.value = event.clientY
+}
+
+onMounted(() => {
+    window.addEventListener('mousemove', setCursorCoords)
+})
+</script>
+
+<template>
+    <div :class="$style.root" />
+</template>
+
+<style lang="scss" module>
+.root {
+    position: fixed;
+    transform: translate(v-bind(x), v-bind(y));
+}
+</style>
