b8c5f11f037d0fa246758cae67b9b486a5221bc8
Levi Ceccato <levi.ceccato@gmail.com>
2022-05-22 11:29:19 +1000

    Add WIP canvas based cursor ring animation

--- src/components/cursor.vue
+++ src/components/cursor.vue
@@ -1,26 +1,75 @@
 <script lang="ts" setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watchEffect } from 'vue'
 
-const WIDTH = 24
-const HEIGHT = 24
+const CURSOR_WIDTH = 4
+const CURSOR_HEIGHT = 4
 
-// Position outside document initially
-const x = ref(-WIDTH)
-const y = ref(-HEIGHT)
+const RING_WIDTH = 24
+const RING_HEIGHT = 24
+
+const cursorX = ref(0)
+const cursorY = ref(0)
+
+const ringX = ref(cursorX.value - (RING_WIDTH / 2))
+const ringY = ref(cursorY.value - (RING_HEIGHT / 2))
+
+const canvas = ref<HTMLCanvasElement | null>(null)
+const ctx = ref<CanvasRenderingContext2D>()
+const canvasWidth = ref(1)
+const canvasHeight = ref(1)
+
+function setCanvasSize(): void {
+    if (!canvas.value) return
+
+    canvas.value.width = window.innerWidth
+    canvasWidth.value = window.innerWidth
+
+    canvas.value.height = window.innerHeight
+    canvasHeight.value = window.innerHeight
+}
 
 function setCursorCoords(event: MouseEvent): void {
-    x.value = event.clientX - (WIDTH / 2)
-    y.value = event.clientY - (HEIGHT / 2)
+    cursorX.value = event.clientX
+    cursorY.value = event.clientY
+}
+
+function drawRing(): void {
+    if (!ctx.value) return
+
+    ctx.value.beginPath()
+
+    ringX.value += (cursorX.value - ringX.value) * 0.1
+    ringY.value += (cursorY.value - ringY.value) * 0.1
+
+    ctx.value.arc(ringX.value, ringY.value, 40, 0, 2 * Math.PI)
+    ctx.value.fillStyle = '#9e356a'
+    ctx.value.fill()
+}
+
+function animateRing(): void {
+    if (!ctx.value) return
+
+    ctx.value.clearRect(0, 0, canvasWidth.value, canvasWidth.value)
+    drawRing()
+    requestAnimationFrame(animateRing)
 }
 
 onMounted(() => {
+    ctx.value = canvas.value?.getContext('2d') || undefined
+    animateRing()
+    setCanvasSize()
     window.addEventListener('mousemove', setCursorCoords)
+    window.addEventListener('resize', setCanvasSize)
 })
 </script>
 
 <template>
-    <div :class="$style.root">
-        <div :class="$style.overlay" />
+    <div>
+        <div :class="$style.cursor" />
+        <canvas
+            ref="canvas"
+            :class="$style.canvas"
+        />
     </div>
 </template>
 
@@ -30,23 +79,24 @@ onMounted(() => {
         cursor: none;
     }
 }
-.root {
-    pointer-events: none;
+.cursor {
     position: fixed;
-    width: calc(v-bind(WIDTH) * 1px);
-    height: calc(v-bind(HEIGHT) * 1px);
-    z-index: 2;
-    background-color: currentColor;
-    border-radius: 1000px;
     filter: invert(1);
     mix-blend-mode: difference;
-    transform: translate(calc(v-bind(x) * 1px), calc(v-bind(y) * 1px));
-}
-.overlay {
-    position: absolute;
-    inset: 0;
-    border-radius: 1000px;
+    transform: translate(calc(v-bind(cursorX) * 1px), calc(v-bind(cursorY) * 1px));
+    pointer-events: none;
+    z-index: 2;
+    width: 4px;
+    height: 4px;
     background-color: currentColor;
-    mix-blend-mode: hue;
+}
+.canvas {
+    pointer-events: none;
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    z-index: 2;
+    filter: invert(1);
+    mix-blend-mode: difference;
 }
 </style>
