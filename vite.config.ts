b982a69e6ae4e1a84f49bc8ecf655c5f28c04802
Levi Ceccato <levi.ceccato@gmail.com>
2022-05-27 21:45:42 +1000

    Generate smaller class names

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,9 @@ export default defineConfig({
         vue({ reactivityTransform: true }),
         ssr(),
     ],
+    css: {
+        modules: {
+            generateScopedName: '[local]_[hash:base64:5]'
+        }
+    },
 })
