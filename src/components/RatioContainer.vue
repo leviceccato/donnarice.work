c39ebe6cd0ed08f2becf3772bf5934a41768002c
Levi Ceccato <levi.ceccato@gmail.com>
2022-07-19 23:13:43 +1000

    Add generic ratio container component

---
+++ src/components/RatioContainer.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+const { ratio } = defineProps<{
+    ratio: number
+}>()
+</script>
+
+<template>
+    <span :class="$style.root">
+        <span
+            :class="$style.wrapper"
+            :style="{ paddingBottom: `${(1 / ratio) * 100}%` }"
+        >
+            <span :class="$style.content">
+                <slot />
+            </span>
+        </span>
+    </span>
+</template>
+
+<style lang="scss" module>
+.root {
+    display: block;
+}
+.wrapper {
+    display: block;
+    position: relative;
+    width: 100%;
+}
+.content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+</style>
